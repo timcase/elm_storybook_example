ee9e90dcd4f660b105c21b3dedec6728319120bf
Tim Case <tim@2drops.net>
2022-01-30 17:00:16 +0700

    Init commit

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    }
+  ],
+  core: {
+    builder: 'webpack5'
+  }
+}
