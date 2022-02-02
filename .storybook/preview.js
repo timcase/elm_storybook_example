236b66c641826b50cc81d6274ec2bf45c8f7514d
Tim Case <tim@2drops.net>
2022-02-02 17:43:12 +0700

    Reconfigure viewports for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,29 @@
 import '../styles/tailwind.css';
 
+const customViewports = {
+  mobile: {
+    name: 'mobile',
+    styles: {
+      width: '375px',
+      height: '667px',
+    },
+  },
+  portrait: {
+    name: 'portrait',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  landscape: {
+    name: 'landscape',
+    styles: {
+      width: '1024px',
+      height: '768px',
+    },
+  }
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -8,4 +32,7 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  viewport: {
+    viewports: customViewports
+  }
 }
